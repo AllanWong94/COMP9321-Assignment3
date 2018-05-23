acaa207f1593ee21e1e9f1b3d9a698a09aa6c650
UNSWZen <lu345808591@gmail.com>
2018-05-23 19:33:43 +1000

    init file for generating Billboard database and word frequency database. (need to run rundb.py before this)

--- init.py
+++ init.py
@@ -1,25 +1,7 @@
-import os
-import csv2dict
-import database
-import re
-import rundb
-# run the database
-# rundb.run()
-
-def init():
-    # store the billboard_lyrics dataset into database
-    filename = "billboard_lyrics_1964-2015.csv"
-    collection_name = re.sub("\.csv", "", filename)
-    collection_name = re.sub("[^a-zA-Z]", "", collection_name)
-    data = csv2dict.csv2dict(filename)
-    status = database.db_save(collection_name, data)
-    if status:
-        print("error")
-    # status = 1 represents error in db_saving.
-    database.db_check(collection_name)
+from BillboardReader import BillboardReader
+from WordFrequency import FrequencyGetter
 
 
-if __name__ == "__main__":
-    # rundb.run()
-    # Can't rundb in init, need to run it before init.py
-    init()
+br = BillboardReader("billboard_lyrics_1964-2015.csv")
+br.run()
+fg = FrequencyGetter(br.data)
