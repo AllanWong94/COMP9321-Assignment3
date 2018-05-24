248dba213b4208c392376f74f4f6f273e5ea2b3d
Zheng Lu <z5135652@cse.unsw.edu.au>
2018-05-24 14:14:15 +1000

    db_get

--- rundb.py
+++ rundb.py
@@ -1,12 +1,29 @@
 import subprocess
+import platform
 
-
-def run():
+def mac_run():
     path = "~/MongoDB/"
     database_init_cmd = "mongod --dbpath "
     database_init_cmd += path
     subprocess.call(database_init_cmd, shell=True)
 
+def win_run():
+    # path = ""
+    pass
+
+def lin_run():
+    pass
+
+def run():
+    sys = platform.system()
+    if sys == "Darwin":
+        mac_run()
+    elif sys == "Windows":
+        win_run()
+    elif sys == "Linux":
+        lin_run()
 
 if __name__ == "__main__":
     run()
+    # print(platform.system())
+    print("database running")
